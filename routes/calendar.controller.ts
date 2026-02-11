8cf1fdd7b30dd83a07ddd7a1f9a81d5b2c077d4d
Varga Barnabas <barnabas_varga@mckinsey.com>
2026-02-11 18:00:43 +0100

    fixed build error

--- routes/calendar.controller.ts
+++ routes/calendar.controller.ts
@@ -3,28 +3,6 @@ import { CalendarService } from "@/services/calendar.service";
 
 export const calendarController = new Hono();
 
-calendarController.post("/events", async (c) => {
-  const body = await c.req.json();
-
-  const service = body.service as string | undefined;
-  const name = body.name as string | undefined;
-  const startDate = body.startDate as string | undefined;
-  const description = body.description as string | undefined;
-  const timeFrame = body.timeFrame as number | undefined;
-
-  return c.json(
-    await CalendarService.createEvent(
-      startDate || "",
-      service || "Appointment",
-      name || "Guest",
-      {
-        description,
-        timeFrame,
-      },
-    ),
-  );
-});
-
 calendarController.get("/events", async (c) => {
   const startDate = c.req.query("startDate") as string | undefined;
   const endDate = c.req.query("endDate") as string | undefined;
